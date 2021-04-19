ed7a0b5af7eda493fb9e534c339f722019915705
“Rishi <riprasan@ucsc.edu>
2021-04-18 17:34:11 -0700

    Added Menu and Play JS files

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ let config = {
     width: 640,
     height: 480
 }
- 
+
 let game = new Phaser.Game(config);
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,7 +1,7 @@
 class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
-    } 
+    }
 
     create() {
         this.add.text(20, 20, "Rocket Patrol Menu");
